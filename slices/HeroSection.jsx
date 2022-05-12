8d2db31e5c34c19fe98144bcd167f65a2e26d4cf
frzrbox <davidefhan@hey.com>
2022-05-11 23:11:33 -1000

    Adds slice markup

--- slices/HeroSection.jsx
+++ slices/HeroSection.jsx
@@ -1,8 +1,23 @@
 export default function HeroSection({ slice }) {
 	return (
 		<div className="hero-section">
+			<img
+				className="hero-section__image"
+				src={slice.primary.hero_image.url}
+				alt={slice.primary.hero_image.alt}
+			/>
 			<div className="hero-section__wrapper">
-				This is the hero section
+				{slice.primary.hero_text && (
+					<h1
+						className={
+							slice.primary.center_title
+								? "hero-section__title--centered"
+								: "hero-section__title"
+						}
+					>
+						{slice.primary.hero_text[0].text}
+					</h1>
+				)}
 			</div>
 		</div>
 	);
--- slices/TextSection.jsx
+++ slices/TextSection.jsx
@@ -1,7 +1,45 @@
+import { PrismicRichText, PrismicLink } from "@prismicio/react";
+
 export default function TextSection({ slice }) {
+	console.log("slice", slice);
+
 	return (
-		<div className="text-section">
-			<div className="text-section__wrapper">Text section</div>
+		<div
+			className={`text-section ${
+				slice.primary.alignment
+					? `text-section--${slice.primary.alignment}`
+					: ""
+			} ${
+				slice.primary.content_width
+					? `text-section--${slice.primary.content_width}`
+					: ""
+			}
+				`}
+		>
+			<div className="text-section__wrapper">
+				<h2 className="text-section__title">
+					{slice.primary.title[0].text}
+				</h2>
+				<PrismicRichText field={slice.primary.content} />
+				{slice.primary.cta_content[0] && (
+					<PrismicLink
+						field={slice.primary.cta_link}
+						className="text-section__link"
+					>
+						{slice.primary.cta_content[0].text}
+					</PrismicLink>
+				)}
+			</div>
+
+			<img
+				className={`text-section-illustration text-section-illustration--${slice.primary.illustration_placement}`}
+				src={
+					slice.primary.illustration_placement === "left"
+						? "/illustrations/mountain.svg"
+						: "/illustrations/mountain-right.svg"
+				}
+				alt=""
+			/>
 		</div>
 	);
 }
