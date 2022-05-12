647a7b92f89632bd62b2fece7503e39d5e0c25e1
frzrbox <davidefhan@hey.com>
2022-05-11 22:41:18 -1000

    Replaces wrapper with fragment

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,16 @@
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "../prismic";
-import styles from "../styles/Home.module.css";
 import Meta from "@/components/Meta";
 import { components } from "@/slices/index";
 
 export default function Home({ page }) {
 	const { page_title, body } = page.data;
 	return (
-		<div className={styles.container}>
+		<>
 			<Meta title={page_title[0].text} />
 			<h1>{page_title[0].text}</h1>
 			<SliceZone slices={body} components={components} />
-		</div>
+		</>
 	);
 }
 
