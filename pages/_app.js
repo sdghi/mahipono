c9ac4575e7289f2faf98d0d75526976eca51908c
frzrbox <davidefhan@hey.com>
2022-05-11 22:40:34 -1000

    Imports scss file

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { PrismicPreview } from "@prismicio/next";
 import { linkResolver, repositoryName } from "../prismic";
 import { createClient } from "../prismic";
 import Header from "@/components/Header";
+import "styles/styles.scss";
 
 export default function App({ Component, pageProps, navigation }) {
 	return (
