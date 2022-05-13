c90e0436504380b8d4f4abe91c3413ced00bb4f4
frzrbox <davidefhan@hey.com>
2022-05-12 23:11:01 -1000

    Styles image with text slice

--- slices/ImageWithText.jsx
+++ slices/ImageWithText.jsx
@@ -3,7 +3,6 @@ import getClassNames from "@/helpers/getClassNames";
 
 export default function ImageWithText({ slice }) {
 	const {
-		content_width,
 		cta_content,
 		cta_link,
 		image,
@@ -11,12 +10,9 @@ export default function ImageWithText({ slice }) {
 		text_content,
 		image_placement,
 		title,
-		alignment,
 	} = slice.primary;
 
 	const homeClassNames = getClassNames("iwt", {
-		content: content_width,
-		alignment,
 		image: image_placement,
 		background: text_background_color,
 	});
@@ -24,18 +20,28 @@ export default function ImageWithText({ slice }) {
 	return (
 		<div className={homeClassNames}>
 			<div className="iwt__wrapper">
-				<img className="iwt__image" src={image.url} alt={image.alt} />
-				<div className="itw__content">
-					<h2 className="iwt__content__title">{title[0].text}</h2>
-					<PrismicRichText field={text_content} />
-					{cta_content[0] && (
-						<PrismicLink
-							className="iwt__content__link"
-							field={cta_link}
-						>
-							{cta_content[0].text}
-						</PrismicLink>
-					)}
+				<div className="iwt__main">
+					<div className="iwt__content">
+						<h2 className="iwt__content__title">{title[0].text}</h2>
+						<div className="iwt__content__body">
+							<PrismicRichText field={text_content} />
+						</div>
+						{cta_content[0] && (
+							<PrismicLink
+								className="iwt__content__link"
+								field={cta_link}
+							>
+								{cta_content[0].text}
+							</PrismicLink>
+						)}
+					</div>
+				</div>
+				<div className="iwt__image-container">
+					<img
+						className="iwt__image-container__image"
+						src={image.url}
+						alt={image.alt}
+					/>
 				</div>
 			</div>
 		</div>
