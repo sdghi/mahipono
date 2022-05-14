ff73145452ed42540294f7f9a9a3ba595c9a875a
frzrbox <davidefhan@hey.com>
2022-05-13 15:53:37 -1000

    Styles accordion

--- slices/Accordion.jsx
+++ slices/Accordion.jsx
@@ -2,6 +2,7 @@ import { PrismicRichText } from "@prismicio/react";
 
 export default function Accordion({ slice }) {
 	const { title } = slice.primary;
+
 	return (
 		<div className="accordion">
 			<div className="accordion__wrapper">
@@ -11,6 +12,12 @@ export default function Accordion({ slice }) {
 						<details className="accordion__items__item" key={index}>
 							<summary className="accordion__items__item__question">
 								{question[0].text}
+								<img
+									className="accordion__items__item__question__icon"
+									src="icons/arrow-down.svg"
+									alt=""
+									role="presentation"
+								/>
 							</summary>
 							<div className="accordion__items__item__answer">
 								<PrismicRichText field={answer} />
