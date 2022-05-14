62908d906cc3a17c3c44cec9d85fd0567b792dd8
frzrbox <davidefhan@hey.com>
2022-05-13 14:47:14 -1000

    Adds pages template

---
+++ pages/[page].jsx
@@ -0,0 +1,38 @@
+import { SliceZone } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
+import { createClient, linkResolver } from "../prismic";
+import Meta from "@/components/meta";
+import { components } from "@/slices/index";
+
+export default function Page({ page }) {
+	const { page_title, body } = page.data;
+	return (
+		<>
+			<Meta title={page_title[0].text} />
+			<SliceZone slices={body} components={components} />
+		</>
+	);
+}
+
+export async function getStaticProps({ params, previewData }) {
+	const client = createClient({ previewData });
+
+	const page = await client.getByUID("page", params.page);
+
+	return {
+		props: { page },
+	};
+}
+
+export async function getStaticPaths() {
+	const client = createClient();
+	const documents = await client.getAllByType("page");
+	const filteredDocs = documents.filter((doc) => {
+		return doc.uid !== "home";
+	});
+
+	return {
+		paths: filteredDocs.map((doc) => prismicH.asLink(doc, linkResolver)),
+		fallback: true,
+	};
+}
