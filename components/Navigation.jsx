d2efcaf35d8c93cf4a6b06a4f2c7b8270858df6a
l7ana <liana@saedesigngroup.com>
2025-06-06 16:49:14 -1000

    adds multilink nav to desktop

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,13 +2,18 @@ import { PrismicLink } from "@prismicio/react";
 import getClassNames from "@/helpers/getClassNames";
 
 export default function Navigation({ navigation }) {
+	// console.log(navigation)
+	navigation.map((item) => (
+		console.log(item.items)
+	))
+	
 	return (
 		<nav className="navigation">
 			<ul className="navigation__items">
-				{navigation.map((item, index) => (
+				{navigation.map((item, index) => 
 					<li
 						className={getClassNames("navigation__items__item", {
-							"is-button": item.primary.type === "button",
+							"is-button": item.primary.type === "button","is-subnav": item.slice_type === "multi_link"
 						})}
 						key={index}
 					>
@@ -18,9 +23,24 @@ export default function Navigation({ navigation }) {
 						>
 							{item.primary.label}
 						</PrismicLink>
+
+					{item.slice_type ==="multi_link" && item.items && item.items.length > 0 && (
+						<ul className="navigation__items navigation__items__item__subnav">
+							{item.items.map((subitem) => (
+							<li className="navigation__items__item__subnav__item" key={index}>
+								<PrismicLink
+									field={subitem.link}
+									className="navigation__items__item__subnav__item__link">
+										{subitem.label}
+									</PrismicLink>
+							</li>
+							))}
+						</ul>
+					)}
 					</li>
-				))}
+				)}
 			</ul>
 		</nav>
 	);
 }
+
